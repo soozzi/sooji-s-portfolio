cda1a68d7f79d69a7c9972b5e519fb302a042e53
오수지 <ohhhhsuzy@naver.com>
2023-01-08 12:02:34 +0900

    git commit

--- 한이음프로젝트/Hanium_Prj/TaskManager/routing.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from TaskManager import consumers
-
-websocket_urlpatterns = [
-    path('ws/test', consumers.Consumer.as_asgi()),
-    path('ws/Drowsiness', consumers.Consumer.as_asgi()),
-    path('ws/TaskManager', consumers.Consumer.as_asgi()),
-    path('ws/Blinking', consumers.Consumer.as_asgi()),
-]
